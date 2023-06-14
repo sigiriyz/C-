6f00d96da2a6269a47391c840202e39e86e409b5
sigiriyz <dima170490@mail.ru>
2023-06-14 19:41:14 +1000

    seminar

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Console.WriteLine("Today is Friday!");// переход на новую сточку
+Console.Write("Today is Friday!")
+
+Console.ReadLine(); //строка
+*/
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your number is: " + number);
+Console.WriteLine($"Your number is: {number}"); // одно и тоже
+
+//int,double (float), bool
